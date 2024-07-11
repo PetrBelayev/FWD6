02345f79941c7df2c96baafb9e01f6bfb37bd188
PetrBelayev <ogganesontennesin@gmail.com>
2024-07-11 21:36:42 +0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Head from 'next/head';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
-      <Head>
-        <title>Comic Page</title>
-        <meta name="description" content="A comic page with server-side rendering" />
-        <meta name="author" content="Your Name" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
