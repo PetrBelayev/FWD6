dc7ae69e972d610f3b79693eaadcf5b93e3825c3
PetrBelayev <ogganesontennesin@gmail.com>
2024-07-11 21:47:07 +0300

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const About: React.FC = () => {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </Layout>
+  );
+};
+
+export default About;
